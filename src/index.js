600cedeb9c5cc769413e8b2ee90b891864ecf0dc
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-03-26 18:08:19 +0530

    Redux Added

--- src/index.js
+++ src/index.js
@@ -4,5 +4,22 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from './store/reducer';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+/* const logger = store => {
+    return next => {
+        return action => {
+            console.log("[Middleware] dispatching", action);
+            const result = next(action);
+            console.log("[Middleware] next state", store.getState())
+            return result;
+        }
+    };
+}; */
+
+const store = createStore(reducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
