9a078cf61170d84a1d93f022e03d4f89f418c808
Sai Sridhar Akula <sridhar.akula001@gmail.com>
2021-06-28 14:17:26 +0530

    Moves Images

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import burgerLogo from "../../assets/images/burger-logo.png";
 import classes from "./Logo.module.css";
 import { Link } from "react-router-dom";
 
@@ -8,7 +7,11 @@ const logo = (props) => (
   <div className={classes.Logo} style={{ height: props.height }}>
     <Link to="/">
       {" "}
-      <img src={burgerLogo} alt="MyBurger" /> <h3>Sai's Burger</h3>
+      <img
+        src={`${process.env.PUBLIC_URL}/assets/images/burger-logo.png`}
+        alt="MyBurger"
+      />{" "}
+      <h3>Sai's Burger</h3>
     </Link>
   </div>
 );
